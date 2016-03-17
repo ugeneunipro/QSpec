81a399c2c34b7c1f05c5b2637b673a6fdd1de442
atiunov <atiunov@unipro.ru>
2016-03-17 15:37:25 +0600

    GTKeyboardDriveLinux: '~' key processing is added

--- src/drivers/GTKeyboardDriverLinux.cpp
+++ src/drivers/GTKeyboardDriverLinux.cpp
@@ -61,7 +61,7 @@ bool GTKeyboardDriver::keyPress(char key, Qt::KeyboardModifiers modifiers)
         break;
     case '~':
         key = '`';
-        XTestFakeKeyEvent(display, XKeysymToKeycode(display, GTKeyboardDriver::key["shift"]), 1, 0);
+        XTestFakeKeyEvent(display, XKeysymToKeycode(display, GTKeyboardDriver::key[Qt::Key_Shift]), 1, 0);
         break;
     case '<':
         key = ',';
@@ -170,7 +170,7 @@ bool GTKeyboardDriver::keyRelease(char key, Qt::KeyboardModifiers modifiers)
     case '~':
         key = '`';
         XTestFakeKeyEvent(display, XKeysymToKeycode(display, key), 0, 0);
-        XTestFakeKeyEvent(display, XKeysymToKeycode(display, GTKeyboardDriver::key["shift"]), 0, 0);
+        XTestFakeKeyEvent(display, XKeysymToKeycode(display, GTKeyboardDriver::key[Qt::Key_Shift]), 0, 0);
         break;
 
     case '<':
