860f712025d6fc1bafc1981e593515ee224262ba
atiunov <atiunov@unipro.ru>
2017-11-14 19:44:48 +0700

    Link to website in the file headers update

--- src/base_dialogs/ColorDialogFiller.cpp
+++ src/base_dialogs/ColorDialogFiller.cpp
@@ -1,7 +1,7 @@
 /**
  * UGENE - Integrated Bioinformatics Tools.
  * Copyright (C) 2008-2017 UniPro <ugene@unipro.ru>
- * http://ugene.unipro.ru
+ * http://ugene.net
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
